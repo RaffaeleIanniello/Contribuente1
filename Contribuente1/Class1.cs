53458a2897f5c779cc1f082861d86db624b50324
Raffaele Ianniello <134380099+RaffaeleIanniello@users.noreply.github.com>
2023-09-01 14:25:41 +0200

    Aggiungere i file di progetto.

---
+++ Contribuente1/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contribuente1
+{
+    public class Contribuente
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public DateTime DataNascita { get; set; }
+        public string CodiceFiscale { get; set; }
+        public char Sesso { get; set; }
+        public string ComuneResidenza { get; set; }
+        public decimal RedditoAnnuale { get; set; }
+
+        public Contribuente(string nome, string cognome, DateTime dataNascita, string codiceFiscale, char sesso, string comuneResidenza, decimal redditoAnnuale)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            DataNascita = dataNascita;
+            CodiceFiscale = codiceFiscale;
+            Sesso = sesso;
+            ComuneResidenza = comuneResidenza;
+            RedditoAnnuale = redditoAnnuale;
+        }
+
+        public decimal CalcolaImposta()
+        {
+            decimal imposta = 0;
+            if (RedditoAnnuale <= 15000)
+            {
+                imposta = RedditoAnnuale * 0.23m;
+            }
+            else if (RedditoAnnuale <= 28000)
+            {
+                imposta = 3450 + (RedditoAnnuale - 15000) * 0.27m;
+            }
+            else if (RedditoAnnuale <= 55000)
+            {
+                imposta = 6960 + (RedditoAnnuale - 28000) * 0.38m;
+            }
+            else if (RedditoAnnuale <= 75000)
+            {
+                imposta = 17220 + (RedditoAnnuale - 55000) * 0.41m;
+            }
+            else
+            {
+                imposta = 25420 + (RedditoAnnuale - 75000) * 0.43m;
+            }
+            return imposta;
+        }
+    }
+}
---
+++ Contribuente1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contribuente1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calcolo imposta da versare:\n");
+
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Cognome: ");
+            string cognome = Console.ReadLine();
+
+            Console.Write("Data di nascita: ");
+            DateTime dataNascita = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null);
+
+            Console.Write("Codice Fiscale: ");
+            string codiceFiscale = Console.ReadLine();
+
+            Console.Write("Sesso (M/F): ");
+            char sesso = char.Parse(Console.ReadLine());
+
+            Console.Write("Comune di residenza: ");
+            string comuneResidenza = Console.ReadLine();
+
+            Console.Write("Reddito Annuale: ");
+            decimal redditoAnnuale = decimal.Parse(Console.ReadLine());
+
+            Contribuente contribuente = new Contribuente(nome, cognome, dataNascita, codiceFiscale, sesso, comuneResidenza, redditoAnnuale);
+
+            decimal impostaDaVersare = contribuente.CalcolaImposta();
+
+            Console.WriteLine("\n riepilogo risultato:");
+            Console.WriteLine($"Contribuente: {contribuente.Nome} {contribuente.Cognome},");
+            Console.WriteLine($"nato il {contribuente.DataNascita.ToString("dd/MM/yyyy")} ({contribuente.Sesso}),");
+            Console.WriteLine($"residente in {contribuente.ComuneResidenza},");
+            Console.WriteLine($"codice fiscale: {contribuente.CodiceFiscale}");
+            Console.WriteLine($"\nReddito dichiarato: {contribuente.RedditoAnnuale:C}");
+
+            Console.WriteLine($"\nIMPOSTA DA VERSARE: {impostaDaVersare:C}");
+
+            Console.ReadLine();
+        }
+    }
+}
